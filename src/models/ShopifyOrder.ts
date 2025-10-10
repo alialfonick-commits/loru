61f8d8b87bd42411b598d7a7af027a270d95e049
mhmdharis <codeaccuracy@gmail.com>
2025-10-10 14:01:27 +0500

    log item ID

--- src/models/ShopifyOrder.ts
+++ src/models/ShopifyOrder.ts
@@ -1,28 +1,38 @@
-// models/ShopifyOrder.ts
 import mongoose, { Schema, Document } from "mongoose";
 
+interface ILineItem {
+  name: string;
+  sku: string;
+}
+
 interface IShopifyOrder extends Document {
-    order_id: string;
-    customer_email: string;
-    customer_name: string;
-    shipping_address: object;
-    addpipe: object;
-    s3_url?: string;
-    qrcode?: string;
-    created_at: Date;
+  order_id: string;
+  customer_email: string;
+  customer_name: string;
+  shipping_address: object;
+  addpipe: object;
+  line_items: ILineItem[];
+  s3_url?: string;
+  qrcode?: string;
+  created_at: Date;
 }
 
 const ShopifyOrderSchema = new Schema<IShopifyOrder>({
-    order_id: { type: String, required: true, unique: true },
-    customer_email: String,
-    customer_name: String,
-    shipping_address: Object,
-    addpipe: Object,
-    s3_url: String,
-    qrcode: String,
-    created_at: { type: Date, default: Date.now },
+  order_id: { type: String, required: true, unique: true },
+  customer_email: String,
+  customer_name: String,
+  shipping_address: Object,
+  addpipe: Object,
+  line_items: [
+    {
+      name: String,
+      sku: String,
+    },
+  ],
+  s3_url: String,
+  qrcode: String,
+  created_at: { type: Date, default: Date.now },
 });
-  
 
 export default mongoose.models.ShopifyOrder ||
   mongoose.model<IShopifyOrder>("ShopifyOrder", ShopifyOrderSchema);
